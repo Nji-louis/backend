95922352c3571d04e0cccf7c4018cd7a321aed86
Nji-louis <nlouisnwufor@gmail.com>
2026-06-07 14:05:32 +0100

    Add Cloudinary uploads

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,28 +1,45 @@
 const express = require("express");
-
 const router = express.Router();
 
-const upload = require("../middleware/uploadMiddleware");
+const multer = require("multer");
+const cloudinary = require("../config/cloudinary");
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage
+});
 
 router.post(
-  "/image",
-  upload.single("image"),
-  (req, res) => {
+    "/",
+    upload.single("image"),
+    async (req, res) => {
 
-    if (!req.file) {
-      return res.status(400).json({
-        success: false,
-        message: "No file uploaded"
-      });
+        try {
+
+            const result = await cloudinary.uploader.upload(
+                `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`,
+                {
+                    folder: "gold-trim-salon"
+                }
+            );
+
+            res.json({
+                success: true,
+                imageUrl: result.secure_url
+            });
+
+        } catch (error) {
+
+            console.log(error);
+
+            res.status(500).json({
+                message: "Upload Failed"
+            });
+
+        }
+
     }
-
-    res.json({
-      success: true,
-      filename: req.file.filename,
-      path: `/uploads/${req.file.filename}`
-    });
-
-  }
 );
 
 module.exports = router;
