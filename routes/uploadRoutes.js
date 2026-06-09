16876d9cf3928085bf455f19c787a92786e690d8
Nji-louis <nlouisnwufor@gmail.com>
2026-06-09 08:47:03 +0100

    Add Cloudinary auth test

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -77,4 +77,28 @@ router.get("/cloudinary-test", async (req, res) => {
 
 });
 
+
+
+router.get("/cloudinary-auth-test", async (req, res) => {
+
+    try {
+
+        const result = await cloudinary.api.ping();
+
+        res.json({
+            success: true,
+            result
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+
+});
+
 module.exports = router;
